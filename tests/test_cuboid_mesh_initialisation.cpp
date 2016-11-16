910c411659c96e23c48ea393b1e418592e8f23cd
ryanpepper <ryan.pepper1@gmail.com>
2016-11-16 12:52:54 +0000

    Add tests

---
+++ tests/test_cuboid_mesh_initialisation.cpp
@@ -0,0 +1,12 @@
+#include <gtest/gtest.h>
+#include "mesh.h"
+
+TEST(mesh_c, initialise_system_global) {
+  GlobalMesh global;
+  LocalMesh local;
+  initialise_system(10, 11, 12, &local, &global);
+  ASSERT_EQ(global.Nx, 10);
+  ASSERT_EQ(global.Ny, 11);
+  ASSERT_EQ(global.Nz, 10);
+}
+
