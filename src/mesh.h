23453d19470007846a51a98acf587df291072b94
ryanpepper <ryan.pepper1@gmail.com>
2016-11-16 12:40:58 +0000

    Update mesh.h

--- src/mesh.h
+++ src/mesh.h
@@ -14,16 +14,4 @@ typedef struct LocalMesh {
   double *m;
 } LocalMesh;
 
-void initialise_system(int Nx, int Ny, int Nz, LocalMesh *local, GlobalMesh *global) {
-  global->Nx = Nx;
-  global->Ny = Ny;
-  global->Nz = Nz;
-  int rank, errorcode;
-  errorcode = MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-  #ifdef DEBUG
-  if(errorcode) {
-    printf("Error in mesh.c: initialise_system: MPI_Comm_rank failed with errorcode %d", 
-           errorcode);
-  }
-  #endif
-}
+void initialise_system(int Nx, int Ny, int Nz, LocalMesh *local, GlobalMesh *global);
